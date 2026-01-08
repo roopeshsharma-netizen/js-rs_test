d62d922f9f26dd28ce8296c4a64876f138ea52e1
roopeshsharma-netizen <roopesh.sharma@ics-global.in>
2026-01-08 09:23:21 +0000

    function

--- javascript/function.js
+++ javascript/function.js
@@ -9,22 +9,51 @@
 // }
 //sayMyName 
 
-function addTwoNumbers(number1,number2){
+// function addTwoNumbers(number1,number2){
 //console.log(number1+number2);// no need to pass console here
 // let result=number1+number2
 // return result
-return number1+number2
-}
-const result= addTwoNumbers(3,4)
+// return number1+number2
+// }
+// const result= addTwoNumbers(3,4)
 // console.log("Result: ", result)
 
-function loginUserMessage(username){
+// function loginUserMessage(username){
     // if(username===undefined){
-    if(!username){
-        console.log("Please enter a username");
-    return
-    }
-return `${username} just logged in`
-}
+    // if(!username){
+        // console.log("Please enter a username");
+    // return
+    // }
+// return `${username} just logged in`
+// }
 //  console.log(loginUserMessage("Roopesh"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+// ******************************************
+//shopping Cart
+function calculateCartPrice(val1,val2, ...num1){ //Rest or spread operator
+return num1
+}
+// console.log(calculateCartPrice(2));
+console.log(calculateCartPrice(200,400,500,2000));
+
+const user={
+    username:"roopesh",
+    prices:199
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and 
+        price is ${anyobject.price}`);
+    
+}
+// handleObject(user)
+handleObject({
+      username:"sam",
+      price:399
+})
+const myNewArray=[200,400,100,600]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,500,1000])); 
