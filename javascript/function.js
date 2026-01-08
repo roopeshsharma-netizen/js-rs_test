c31cec75510fa6b4d7799b1ae5986dccddc2d317
roopeshsharma-netizen <roopesh.sharma@ics-global.in>
2026-01-08 08:52:13 +0000

    function

--- javascript/function.js
+++ javascript/function.js
@@ -10,6 +10,21 @@
 //sayMyName 
 
 function addTwoNumbers(number1,number2){
-console.log(number1+number2);
+//console.log(number1+number2);// no need to pass console here
+// let result=number1+number2
+// return result
+return number1+number2
 }
-addTwoNumbers(3,4)
+const result= addTwoNumbers(3,4)
+// console.log("Result: ", result)
+
+function loginUserMessage(username){
+    // if(username===undefined){
+    if(!username){
+        console.log("Please enter a username");
+    return
+    }
+return `${username} just logged in`
+}
+//  console.log(loginUserMessage("Roopesh"))
+console.log(loginUserMessage())
