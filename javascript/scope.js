1c77bde3f0c56380dbfa373a60ed01518624b7a9
roopeshsharma-netizen <roopesh.sharma@ics-global.in>
2026-01-08 10:47:22 +0000

    scope

---
+++ javascript/scope.js
@@ -0,0 +1,38 @@
+// let a=10
+// const b=20
+// var c=30
+// var c=300
+/*let a=300
+if(true){
+let a=10
+const b=20
+// var c=30  
+console.log("INNER: ",a);
+  
+}
+*/
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// ******************************
+ function one(){
+    const username="roopesh"
+    function two(){
+        const website="youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    // two()
+//  }
+//  one()
+ if(true){
+    const username="roopesh"
+    if (username==="roopesh"){
+        const website="youtube"
+        console.log(username + website);
+    }
+    console.log(website)
+ }
+ console.log(username);
