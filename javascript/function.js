8b0f0f552f34157929be8cd5fb0a7efb1ea5cdff
roopeshsharma-netizen <roopesh.sharma@ics-global.in>
2026-01-08 07:14:57 +0000

    function

---
+++ javascript/function.js
@@ -0,0 +1,15 @@
+// function sayMyName(){
+// console.log("R");
+// console.log("O");
+// console.log("O");
+// console.log("P");
+// console.log("E");
+// console.log("S");
+// console.log("H");
+// }
+//sayMyName 
+
+function addTwoNumbers(number1,number2){
+console.log(number1+number2);
+}
+addTwoNumbers(3,4)
